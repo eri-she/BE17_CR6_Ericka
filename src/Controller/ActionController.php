c12861babf8bef03cc61a3feb3f6828edebc703b
Ericka Angeles <ericka.angelesgarcia@gmail.com>
2022-12-02 13:01:15 +0100

    added index

--- src/Controller/ActionController.php
+++ src/Controller/ActionController.php
@@ -2,19 +2,21 @@
 
 namespace App\Controller;
 
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\Name;
 class ActionController extends AbstractController
 {
     /**
      * @Route("/", name="index")
      */
-    public function index(): Response
-    {
+    public function index(ManagerRegistry $doctrine): Response
+    { $events = $doctrine->getRepository(Name::class)->findAll();
         return $this->render('action/index.html.twig', [
-            'controller_name' => 'ActionController',
+           
+            'events' => $events
         ]);
     }
 
